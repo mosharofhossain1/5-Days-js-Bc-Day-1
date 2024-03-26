12ece2b3ce64ddcc21d261551d1de72529ffdf25
Mosharof <md5470415@gmail.com>
2024-03-26 11:18:40 +0600

    unite three is complate

--- Day-1/unite-3.js
+++ Day-1/unite-3.js
@@ -1,1 +1,39 @@
-console.log('unite 3 is comming')
+// Unite 3 :- Math Object 
+
+    // 1. Math.PI()
+    console.log(Math.PI);
+    console.log(Math.PI.toFixed(5));
+    console.log(parseFloat((Math.PI).toFixed(5)));
+
+    // 2. Math.E()
+    console.log(Math.E);
+    console.log(parseFloat(Math.E.toFixed(2)))
+    console.log(Math.E.toFixed(5));
+
+    // 3. Math.abs()
+    const number = -98;
+    const absValue = Math.abs(number);
+    console.log(absValue);
+    console.log(Math.abs(-34));
+
+    // 4. Math.floor()
+    let num = 6.1;
+    let floorValue = Math.floor(num);
+    console.log(floorValue);
+
+    // 5. Math.ceil()
+
+    let ceilValue = Math.ceil(num);
+    console.log(ceilValue);
+
+    // 6. Math.round()
+    let roundValue = Math.round(num);
+    console.log(roundValue);
+
+    // 7. Math.pow()
+    console.log(Math.pow(8,2))
+
+    // 8. Math.sqr()
+    console.log(Math.sqrt(81));
+
+    
